c86d526dcd32f92765444c79e7f88db9d32e929c
egbakou <egbakoukodjolaurent@gmail.com>
2019-08-04 23:27:35 +0000

    Added custom exception.

---
+++ RESTCountries.NET/Helpers/CountryNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿/*
+CountryNotFoundException.cs
+04/08/2019 21:44:30
+Kodjo Laurent Egbakou
+*/
+
+using System;
+
+namespace AppREstCountries.Helpers
+{
+    /// <summary>
+    /// Defines a <see cref="CountryNotFoundException" />
+    /// </summary>
+    public class CountryNotFoundException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CountryNotFoundException"/> class.
+        /// </summary>
+        /// <param name="field">The field<see cref="string"/></param>
+        /// <param name="value">The value<see cref="String"/></param>
+        public CountryNotFoundException(string field, String value)
+        : base(String.Format("Country not found using field '{0}' with value '{1}.", field, value))
+        {
+        }
+    }
+}
