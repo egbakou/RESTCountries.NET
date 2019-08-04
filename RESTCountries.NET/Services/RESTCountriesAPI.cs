11ee4b228a00d299532c4a16c00dd9b2547a4f7f
egbakou <egbakoukodjolaurent@gmail.com>
2019-08-04 23:35:58 +0000

    RESTCountriesAPI class created.

---
+++ RESTCountries.NET/Services/RESTCountriesAPI.cs
@@ -0,0 +1,21 @@
+﻿/*
+RESTCountriesAPI.cs
+04/08/2019 21:51:32
+Kodjo Laurent Egbakou
+*/
+
+using RestSharp;
+
+namespace RESTCountries.Services
+{
+    /// <summary>
+    /// Defines a <see cref="RESTCountriesAPI" />
+    /// </summary>
+    public class RESTCountriesAPI
+    {
+        /// <summary>
+        /// Defines a RestClient static object.
+        /// </summary>
+        private static readonly RestClient client = new RestClient();
+    }
+}
