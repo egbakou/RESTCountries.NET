472895be83b6bb6bce96f862d6de187081371c8c
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-12-11 17:05:01 +0100

    refactor: clean up

--- tests/RESTCountries.NET.Benchmark/Program.cs
+++ tests/RESTCountries.NET.Benchmark/Program.cs
@@ -1,12 +1,4 @@
 ﻿using BenchmarkDotNet.Running;
 using RESTCountries.NET.Benchmark;
 
-// CountryData.Standard is not optimized during build,
-// So to avoid benchmark errors, we need to disable the OptimizationValidator
-/*var config = new ManualConfig()
-    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
-    .AddValidator(JitOptimizationsValidator.DontFailOnError)
-    .AddLogger(ConsoleLogger.Default)
-    .AddColumnProvider(DefaultColumnProviders.Instance);*/
-
 BenchmarkRunner.Run<RestCountriesServiceBenchmark>();
