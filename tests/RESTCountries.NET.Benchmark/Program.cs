d33d254850f0444faa93a07d4daf0f700c5583fd
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-12-11 17:02:58 +0100

    perf: update benchmark code

--- tests/RESTCountries.NET.Benchmark/Program.cs
+++ tests/RESTCountries.NET.Benchmark/Program.cs
@@ -1,16 +1,12 @@
-﻿using BenchmarkDotNet.Columns;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Loggers;
-using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Validators;
+﻿using BenchmarkDotNet.Running;
 using RESTCountries.NET.Benchmark;
 
 // CountryData.Standard is not optimized during build,
 // So to avoid benchmark errors, we need to disable the OptimizationValidator
-var config = new ManualConfig()
+/*var config = new ManualConfig()
     .WithOptions(ConfigOptions.DisableOptimizationsValidator)
     .AddValidator(JitOptimizationsValidator.DontFailOnError)
     .AddLogger(ConsoleLogger.Default)
-    .AddColumnProvider(DefaultColumnProviders.Instance);
+    .AddColumnProvider(DefaultColumnProviders.Instance);*/
 
-BenchmarkRunner.Run<RestCountriesServiceBenchmark>(config);
+BenchmarkRunner.Run<RestCountriesServiceBenchmark>();
--- tests/RESTCountries.NET.Benchmark/RestCountriesServiceBenchmark.cs
+++ tests/RESTCountries.NET.Benchmark/RestCountriesServiceBenchmark.cs
@@ -1,38 +1,14 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Columns;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Jobs;
-using CountryData.Standard;
 using RESTCountries.NET.Services;
-using Country = RESTCountries.NET.Models.Country;
-using CountryDataCountry = CountryData.Standard.Country;
+using RESTCountries.NET.Models;
 
 namespace RESTCountries.NET.Benchmark;
 
 [MemoryDiagnoser]
 [MediumRunJob]
-[Config(typeof(Config))]
 public class RestCountriesServiceBenchmark
 {
-    private class Config : ManualConfig
-    {
-        public Config()
-        {
-            AddJob(Job.MediumRun);
-            AddColumn(new TagColumn("Nuget", name =>
-                name.StartsWith("GetAll") ? "RESTCountries.NET 3.0.0" : "CountryData.Standard 1.3.0"));
-        }
-    }
-    
-    [Benchmark]
-    [Arguments("RESTCountries.NET")]
-    public List<Country> GetAllCountries() => RestCountriesService.GetAllCountries().ToList();
 
     [Benchmark]
-    [Arguments("CountryData.Standard")]
-    public List<CountryDataCountry> GetCountryData()
-    {
-        var helper = new CountryHelper();
-        return helper.GetCountryData().ToList();
-    }
+    public List<Country> GetAllCountries() => RestCountriesService.GetAllCountries().ToList();
 }
